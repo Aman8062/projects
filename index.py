8301ae3f8f8eeccea4c4f1f10946098e72c3b202
aman <amankumar977380@gmail.com>
2025-02-20 11:41:06 +0530

    this is second commit

--- index.py
+++ index.py
@@ -1,2 +1,20 @@
 # print("hello everyone we are learners")
 
+
+
+# num1 = int(input("Enter first number "))
+# num2= int(input("Enter the second number "))
+
+# num3 = num1+num2
+# print(num3)
+
+
+
+# num1 = int(input("Enter first number "))
+# print(type(num1))
+
+# num2 = input("Enter the second number ")
+# print(type(num2))
+
+
+# data types in python practical 
