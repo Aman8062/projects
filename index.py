80176259fe8d35fba63b839037f803ed1b6b8e50
aman <amankumar977380@gmail.com>
2025-02-22 09:36:00 +0530

    operators are completed

--- index.py
+++ index.py
@@ -77,11 +77,74 @@ c=a+b
 
 
 # Logical Operators in Python
-a = True 
-b = False
-print(a and b)  
-print(a or b)
-print(not a)
+# a = True 
+# b = False
+# print(a and b)  
+# print(a or b)
+# print(not a)
+
+
+# Bitwise Operators in Python
+
+# Bitwise NOT
+# Bitwise Shift
+# Bitwise AND
+# Bitwise XOR
+# Bitwise OR
+
+# a = 10
+# b = 4
+
+# print(a & b)
+# print(a | b)
+# print(~a)
+# print(a ^ b)
+# print(a >> 2)
+# print(a << 2)
 
 
 
+# Assignment Operators in Python
+
+# a = 10
+# b = a
+# print(b)
+# b += a
+# print(b)
+# b -= a
+# print(b)
+# b *= a
+# print(b)
+# b <<= a
+# print(b)
+
+
+# Identity Operators in Python
+# is          True if the operands are identical 
+# is not      True if the operands are not identical 
+
+# a = 10
+# b = 20
+# c = a
+
+# print(a is not b)
+# print(a is c)
+
+# Membership Operators in Python
+
+# in            True if value is found in the sequence
+# not in        True if value is not found in the sequence
+
+# x = 24
+# y = 20
+# list = [10, 20, 30, 40, 50]
+
+# if (x not in list):
+#     print("x is NOT present in given list")
+# else:
+#     print("x is present in given list")
+
+# if (y in list):
+#     print("y is present in given list")
+# else:
+#     print("y is NOT present in given list")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+# if (y in list):
+#     print("y is present in given list")
+# else:
+#     print("y is NOT present in given list")
